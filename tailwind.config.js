3312147ea14c4cc4171486ce18bdf4c987e65866
amrancz <adam@amran.cz>
2021-04-01 14:35:05 +0200

    Update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    options: {
+      safelist: ['bg-*-*', 'text-*-*', 'from-*-*', 'to-*-*'],
+    },
+  },
 
   darkMode: 'class', // or 'media' or 'class'
   theme: {
