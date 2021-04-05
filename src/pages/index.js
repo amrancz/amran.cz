aefac7c6b1d2aad987b27567514c6b4871030e93
amrancz <adam@amran.cz>
2021-04-05 09:02:26 +0200

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,14 +18,21 @@ class Index extends React.Component {
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <div className={'flex flex-col lg:flex-row lg:items-stretch lg:space-x-8 space-y-8 lg:space-y-0'}>
-          <div className={'bg-gradient-to-br from-green-500 to-blue-600 text-white lg:flex-1 rounded-2xl p-6 md:p-12 space-y-4 md:space-y-6'}>
+          <div className={'bg-gradient-to-br justify-center items-center text-center from-green-500 to-blue-600 text-white lg:flex-1 rounded-2xl p-6 md:p-12 space-y-4 md:space-y-6'}>
             <div
-            className={'w-32 h-32 flex rounded-2xl overflow-visible bg-green-800 hidden lg:block'}>
+            className={'w-32 h-32 rounded-full mx-auto bg-green-800 hidden lg:block'}>
               <StaticImage 
                 src={'../images/portraitSmall.jpg'}
                 alt='Portrait of Adam'
+                style={{
+                  justifySelf: 'center',
+                  textAlign: 'center',
+                  borderRadius: '10rem',
+                  overflow: 'hidden',
+                  backgroundColor: 'transparent',
+                }}
                 imgStyle={{
-                  borderRadius: '1rem',
+                  borderRadius: '10rem',
                   overflow: 'hidden'
                 }}
               ></StaticImage>
@@ -35,7 +42,7 @@ class Index extends React.Component {
                   Product Designer at <CustomLink link={'https://www.productboard.com'} target={"_blank"} text={`Productboard`}></CustomLink> <br />
                   Making <CustomLink link={'https://www.untools.co'} target={"_blank"} text={`Untools.co`}></CustomLink> on the side.
             </p>
-            <div className={'py-2 flex flex-col md:flex-row lg:flex-row flex-shrink space-y-4 space-x-0 md:space-y-0 md:space-x-4'}>
+            <div className={'py-2 flex flex-col justify-center sm:flex-row md:flex-row space-y-4 space-x-0 sm:space-y-0 sm:space-x-4'}>
               <Button primary={true} link={'/work'} text={'See my work'} textColor={'green-800'}></Button>
               <Button link={'/about'} text={'Learn more about me'}></Button>
             </div>
