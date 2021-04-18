69e6a894c5c739bbc7beab8c8f36c09c9ac26ded
amrancz <adam@amran.cz>
2021-04-18 09:10:45 +0200

    Fix case study links and 404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,16 +7,15 @@ import CustomLink from '../components/CustomLink'
 class NotFoundPage extends React.Component {
   render() {
     return (
-      <Layout location={this.props.location} spacing={'12'}>
+      <Layout location={this.props.location} spacing={'4'}>
         <SEO title="404: Not Found" />
-        <h1 className={'flex justify-center'}>This page isn't here</h1>
-        <h1 className={'flex justify-center'}>¯\_(ツ)_/¯</h1>
-        <p  className={'flex justify-center'}>
-          If you got there via a link on this page, <Link to={`https://twitter.com/amrancz`} className={'font-semibold tracking-tighter hover:text-green-400'}>
-          please let me know
-        </Link>.
-          Otherwise, your best bet is going to the <Link to={`/`} className={'font-semibold tracking-tighter hover:text-green-400'}>homepage
-        </Link>.
+        <h1 className={'flex justify-center pb-8'}>This page isn't here</h1>
+        <h1 className={'flex justify-center pb-12'}>¯\_(ツ)_/¯</h1>
+        <p className={'text-center'}>
+          If you got there via a link on this page, <CustomLink link={`mailto:adam@amran.cz?subject=Yo, there's a broken link on amran.cz`} target={'_blank'} style={'themeable'}  text={'let me know'}></CustomLink>
+        </p>
+        <p className={'text-center'}> 
+          Otherwise, your best bet is going to the <CustomLink link={'/'} style={'themeable'}  text={'homepage'}></CustomLink>
         </p>
         
       </Layout>
